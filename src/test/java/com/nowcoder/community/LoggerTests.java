f8705ac81d798cbc5c51473f36735b74b26e6b03
FlyHit <2692217528@qq.com>
2021-08-08 20:15:28 +0800

    日志的使用

---
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    void testLogger() {
+        System.out.println(LOGGER.getName());
+        LOGGER.debug("debug log");
+        LOGGER.info("info log");
+        LOGGER.warn("warn log");
+        LOGGER.error("error log");
+    }
+}
