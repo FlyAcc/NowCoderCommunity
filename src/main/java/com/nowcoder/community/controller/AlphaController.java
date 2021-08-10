b5de005396eee889da64efb9c6c3c1a856ca371b
FlyHit <2692217528@qq.com>
2021-08-10 16:19:18 +0800

    生成验证码
    - 使用cookies和session
    - 使用session保存验证码，并使用response的输出流将验证码传输到浏览器

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,14 +1,17 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -137,5 +140,57 @@ public class AlphaController {
         emp.put("salary", 800012);
         list.add(emp);
         return list;
+    }
+
+    // cookies示例
+
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // cookie只能存一小段数据，一般是字符串
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        // 设置cookie生效范围
+        cookie.setPath("/community/alpha");
+        // 设置cookie失效时间，默认是存入内存，关掉浏览器即失效（值为-1）
+        cookie.setMaxAge(60 * 10);
+        // 发送cookie
+        response.addCookie(cookie);
+
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") /*获取name为code的cookie*/
+                                    String code) {
+//        由于服务器指定 Cookie 后，浏览器的每次请求都会携带 Cookie 数据，会带来额外的性能开销（尤其是在移动环境下）
+//        同时某些隐私数据存在客户端可能会带来安全问题
+        System.out.println(code);
+        return "get cookie";
+    }
+
+    /*
+    session示例：使用session，数据可以存在服务端比较安全，但同时也对服务器压力也提高了
+    分布式部署：黏性session（同一个ip固定分给一个服务器，影响负载均衡），同步session（一个服务器的session同步给其他服务器，
+    影响服务器性能，同时服务器之间相互不独立，影响部署），共享session（单独一个服务器存储session，但是对该服务器性能要求高
+    同时该服务器宕机会影响其他服务器
+    数据库做集群，session数据保存在数据库中，服务器从数据库中读取数据，但是数据库读取性能比较差（硬盘）
+    可以使用redis解决该问题（IMDB，in memory database，是一种nosql，key/value数据库
+     */
+
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id", 1);
+        session.setAttribute("name", "Test");
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
     }
 }
--- src/main/java/com/nowcoder/community/controller/LoggingController.java
+++ src/main/java/com/nowcoder/community/controller/LoggingController.java
@@ -1,8 +1,11 @@
 package com.nowcoder.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +13,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoggingController implements CommunityConstant {
     @Autowired
     private UserService userService;
+    @Autowired
+    private Producer producer;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingController.class);
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -60,5 +73,24 @@ public class LoggingController implements CommunityConstant {
         }
 
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        // 生成验证码
+        String text = producer.createText();
+        BufferedImage image = producer.createImage(text);
+
+        // 验证码存入session
+        session.setAttribute("kaptcha", text);
+
+        // 将图片输出给浏览器，该输出流由spring维护，不需要关闭
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            LOGGER.error("响应验证码失败: " + e.getMessage());
+        }
     }
 }
