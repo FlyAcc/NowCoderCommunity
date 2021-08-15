f0d9e2c41b5e30b08f5d8915ead8f54061e2dc79
FlyHit <2692217528@qq.com>
2021-08-15 13:25:02 +0800

    使用Spring进行事务管理
    - 声明式：@Transactional
    - 编程式：TransactionTemplate

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,18 +1,35 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 @Scope("singleton") // 默认是singleton，prototype每次获取均新实例化
 public class AlphaService {
     @Autowired
     private AlphaDao alphaDao;
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService() {
         System.out.println("实例化AlphaService");
@@ -30,5 +47,63 @@ public class AlphaService {
 
     public String find() {
         return alphaDao.select();
+    }
+
+    /*
+    使用事务管理，当事务出现异常时，可以安全回滚
+     propagation: 事务传播机制，事物之间可能互相调用
+     REQUIRED：使用当前事务（外部事务），如果不存在则创建新事务
+     REQUIRED_NEW: 创建一个新事务，并且暂停当前事务（外部事务）
+     NESTED：如果当前存在事务（外部事务），则嵌套在该事务中执行（独立的提交和回滚），否则就和REQUiRED一样
+     */
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // 新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setCreateTime(new Date());
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        userMapper.insertUser(user);
+
+        // 新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello！");
+        post.setContent("新人报到");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        Integer.valueOf("abc");
+        return "ok";
+    }
+
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        return transactionTemplate.execute(status -> {
+            // 新增用户
+            User user = new User();
+            user.setUsername("belta");
+            user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+            user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+            user.setEmail("belta@qq.com");
+            user.setCreateTime(new Date());
+            user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+            userMapper.insertUser(user);
+
+            // 新增帖子
+            DiscussPost post = new DiscussPost();
+            post.setUserId(user.getId());
+            post.setTitle("Hello！");
+            post.setContent("新人报到");
+            post.setCreateTime(new Date());
+            discussPostMapper.insertDiscussPost(post);
+
+            Integer.valueOf("abc");
+            return "ok";
+        });
     }
 }
