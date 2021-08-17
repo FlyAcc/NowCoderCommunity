dfaad9f640be15f2c2df40554957e3c1e0b629c6
FlyHit <2692217528@qq.com>
2021-08-17 14:35:18 +0800

    显示消息列表（表现层）

---
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -0,0 +1,63 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.Message;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.MessageService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MessageController {
+    @Autowired
+    private MessageService messageService;
+    @Autowired
+    private HostHolder hostHolder;
+    @Autowired
+    private UserService userService;
+
+    /*
+    私信列表需要显示的信息有：总的未读数，所有私信会话（只显示最新的一条私信内容），每个会话中的所有消息及其数量和未读数
+     */
+    @RequestMapping(path = "/letter/list", method = RequestMethod.GET)
+    public String getLetterList(Model model, Page page) {
+        User user = hostHolder.getUser();
+        // 分页信息
+        page.setLimit(5);
+        page.setPath("/letter/list");
+        page.setRows(messageService.findConversationCount(user.getId()));
+        // 会话列表
+        List<Message> conversationsList = messageService.findConversations(user.getId(), page.getOffset(), page.getLimit());
+        List<Map<String, Object>> conversationInfos = new ArrayList<>();
+        if (conversationsList != null) {
+            for (Message message : conversationsList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("conversation", message);
+                map.put("unreadCount", messageService.findLetterUnreadCount(user.getId(), message.getConversationId()));
+                map.put("letterCount", messageService.findLetterCount(message.getConversationId()));
+                // target即私信的对象
+                int targetId = user.getId() == message.getFromId() ? message.getToId() : message.getFromId();
+                map.put("target", userService.findUserById(targetId));
+
+                conversationInfos.add(map);
+            }
+        }
+        model.addAttribute("conversations", conversationInfos);
+
+        // 查询所有未读消息的数量
+        int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+        model.addAttribute("letterUnreadCount", letterUnreadCount);
+
+        return "/site/letter";
+    }
+}
--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -5,20 +5,53 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+/**
+ * 私信列表需要显示的信息有：总的未读数，所有私信会话及数量（只显示最新的一条私信内容），每个会话中的所有消息及其数量和未读数
+ */
 @Mapper
 public interface MessageMapper {
-    // 查询当前用户的会话列表，针对每个会话只返回一条最新的私信
+    /**
+     * 查询当前用户的会话列表，针对每个会话只返回一条最新的私信
+     *
+     * @param userId 用户id
+     * @param offset
+     * @param limit
+     * @return 会话列表
+     */
     List<Message> selectConversations(int userId, int offset, int limit);
 
-    // 查询当前用户的会话数量
+    /**
+     * 查询当前用户的所有会话的数量
+     *
+     * @param userId 用户id
+     * @return 会话数量
+     */
     int selectConversationCount(int userId);
 
-    // 查询某个会话所包含的私信列表
+    /**
+     * 查询某个会话所包含的所有消息
+     *
+     * @param conversationId 会话id
+     * @param offset
+     * @param limit
+     * @return 一个会话内的所有消息
+     */
     List<Message> selectLetters(String conversationId, int offset, int limit);
 
-    // 查询某个会话所包含的私信数量
+    /**
+     * 查询某个会话所包含的消息数量
+     *
+     * @param conversationId 会话id
+     * @return 会话内的消息数量
+     */
     int selectLetterCount(String conversationId);
 
-    // 查询未读私信数量
+    /**
+     * 查询未读消息数量，若会话id为null，则查询全部未读消息的数量
+     *
+     * @param userId         用户id
+     * @param conversationId 会话id
+     * @return 未读消息数量
+     */
     int selectLetterUnreadCount(int userId, String conversationId);
 }
